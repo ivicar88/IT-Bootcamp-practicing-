4da4de6717964a54f52e263c43a4b9ba7a350f05
ivicar88 <ivica.rakovic1988.sd@gmail.com>
2019-03-22 20:59:29 +0100

    Dodao sam registracionu formu

---
+++ regForma/RegUrl.java
@@ -0,0 +1,32 @@
+package regForma;
+
+public class RegUrl {
+	
+	public static final String url = "http://newtours.demoaut.com/";
+	public static final String reg = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[2]/td/table/tbody/tr/td[2]/a";
+	public static final String firstn = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/input";
+	public static final String ime = "Ivica";
+	public static final String lastn = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[3]/td[2]/input";
+	public static final String prezime = "Rakovic";
+	public static final String phone = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[4]/td[2]/input";
+	public static final String telefon = "066/66/66/666";
+	public static final String email = "//*[@id=\"userName\"]";
+	public static final String mejl = "ivica.rakovic@gmail.com";
+	public static final String address = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[7]/td[2]/input";
+	public static final String adresa = "Kosmajska";
+	public static final String city = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/input";
+	public static final String grad = "Beograd";
+	public static final String province = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[10]/td[2]/input";
+	public static final String oblast = "Sumadija";
+	public static final String zipcode = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[11]/td[2]/input";
+	public static final String postkod = "11032";
+	public static final String username = "//*[@id=\"email\"]";
+	public static final String korisIme = "ivicar88";
+	public static final String password = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[15]/td[2]/input";
+	public static final String confpassword = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[16]/td[2]/input";
+	public static final String lozinka = "svastanesto";
+	public static final String submit = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[18]/td/input";
+	
+	
+
+}
---
+++ regForma/TestiranjeRegForme.java
@@ -0,0 +1,45 @@
+package regForma;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestiranjeRegForme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		try {
+			driver.get(RegUrl.url);
+			driver.findElement(By.xpath(RegUrl.reg)).click();;
+			driver.findElement(By.xpath(RegUrl.firstn)).sendKeys(RegUrl.ime);
+			driver.findElement(By.xpath(RegUrl.lastn)).sendKeys(RegUrl.prezime);
+			driver.findElement(By.xpath(RegUrl.phone)).sendKeys(RegUrl.telefon);
+			driver.findElement(By.xpath(RegUrl.email)).sendKeys(RegUrl.mejl);
+			driver.findElement(By.xpath(RegUrl.address)).sendKeys(RegUrl.adresa);
+			driver.findElement(By.xpath(RegUrl.city)).sendKeys(RegUrl.grad);
+			driver.findElement(By.xpath(RegUrl.province)).sendKeys(RegUrl.oblast);
+			driver.findElement(By.xpath(RegUrl.zipcode)).sendKeys(RegUrl.postkod);
+			Select country = new Select(driver.findElement((By.xpath("//select[@name='country']"))));
+	           country.selectByVisibleText("SERBIA");
+			driver.findElement(By.xpath(RegUrl.username)).sendKeys(RegUrl.korisIme);
+			driver.findElement(By.xpath(RegUrl.password)).sendKeys(RegUrl.lozinka);
+			driver.findElement(By.xpath(RegUrl.confpassword)).sendKeys(RegUrl.lozinka);
+			driver.findElement(By.xpath(RegUrl.submit)).click();
+		} catch (Exception ex) {
+			System.out.println(ex.toString());
+			
+		} finally {
+			driver.quit();
+		}
+
+	}
+
+}
